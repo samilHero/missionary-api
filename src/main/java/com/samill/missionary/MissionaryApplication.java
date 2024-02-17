d878084b8b9474205c0487701e97b0d85015aadc
eden <ydu3328@naver.com>
2024-02-17 22:07:56 +0900

    스프링 부트 프로젝트 생성

---
+++ src/main/java/com/samill/missionary/MissionaryApplication.java
@@ -0,0 +1,13 @@
+package com.samill.missionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samill/missionary/MissionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samill.missionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
