069ada0329c11ed8a9a9c90f83518986c508169e
eden <ydu3328@naver.com>
2024-04-20 15:17:58 +0900

    fix:  ChurchRepositoryTests AutoConfigureTestDatabase추가

--- src/test/java/com/samill/missionary_backend/church/repository/ChurchRepositoryTests.java
+++ src/test/java/com/samill/missionary_backend/church/repository/ChurchRepositoryTests.java
@@ -10,12 +10,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(RepositoryTestConfig.class)
 class ChurchRepositoryTests {
 
