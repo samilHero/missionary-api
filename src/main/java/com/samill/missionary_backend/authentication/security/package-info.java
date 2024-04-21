abd942424696437ce908e24327885db75e395caf
onestar94 <lwnddms029@gmail.com>
2024-04-21 19:38:42 +0900

    test: spring security test code 작성

---
+++ src/main/java/com/samill/missionary_backend/authentication/security/package-info.java
@@ -0,0 +1,2 @@
+@org.springframework.modulith.NamedInterface("security")
+package com.samill.missionary_backend.authentication.security;
---
+++ src/main/java/com/samill/missionary_backend/gateway/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+        allowedDependencies = {
+                "authentication::security",
+        }
+)
+package com.samill.missionary_backend.gateway;
