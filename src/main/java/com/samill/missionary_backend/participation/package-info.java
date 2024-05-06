67dcb5c254f4b5c3d0a4fe9d05236162f3ea93f9
kimyeojin <matilda94616@gmail.com>
2024-05-06 14:24:17 +0900

    feat: common:util > package-info 추가

--- src/main/java/com/samill/missionary_backend/participation/package-info.java
+++ src/main/java/com/samill/missionary_backend/participation/package-info.java
@@ -7,6 +7,7 @@
                 "common::entity",
                 "common::enums",
                 "common::exception",
+                "common::dto",
                 "common::util"
         }
 )
