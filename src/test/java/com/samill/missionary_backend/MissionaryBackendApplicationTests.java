89af70a16d506b9ae1b39e6be8eed145cc43f167
eden <ydu3328@naver.com>
2024-05-21 23:56:26 +0900

    refactor: 스프링부트 테스트 코드 추상클래스 생성

--- src/test/java/com/samill/missionary_backend/MissionaryBackendApplicationTests.java
+++ src/test/java/com/samill/missionary_backend/MissionaryBackendApplicationTests.java
@@ -1,14 +1,12 @@
 package com.samill.missionary_backend;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.modulith.core.ApplicationModule;
 import org.springframework.modulith.core.ApplicationModules;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
-@SpringBootTest
 class MissionaryBackendApplicationTests {
 
     ApplicationModules modules = ApplicationModules.of(MissionaryBackendApplication.class);
