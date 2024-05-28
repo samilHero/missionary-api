d358d7d6df12d1bc3cae728cb8dce51a470051a9
eden <ydu3328@naver.com>
2024-05-29 01:44:23 +0900

    build: 그래들 aop 추가 및 어노테이션 적용

--- src/main/java/com/samill/missionary_backend/MissionaryBackendApplication.java
+++ src/main/java/com/samill/missionary_backend/MissionaryBackendApplication.java
@@ -3,12 +3,14 @@ package com.samill.missionary_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableAspectJAutoProxy
 @EnableScheduling
 public class MissionaryBackendApplication {
 
